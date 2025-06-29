9e4467f1eb377f47b15461ad449f432f8619b699
onkar123-cmd <omhui1234@gmail.com>
2025-06-29 15:34:31 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>+91-9339152936</li>
+                    <li>+91-8918942477</li>
                     <li>aritrakundu864@gmail.com</li>
                 </ul>
             </div>
